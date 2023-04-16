f687c8fd6f2f631d70594b4d57d6b2f6a07a7e29
Julian Finelli <julianfinelli97@gmail.com>
2023-04-16 20:10:47 -0300

    detalles

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,17 +1,28 @@
 import { Link, useNavigate } from "react-router-dom"
 import { ItemCountEventos } from "../ItemCountEventos"
-import { ColorPicker } from "../ColorPicker/ColorPicker"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext, useState } from "react";
 import "./itemDetail.scss"
 import { CartContext } from "../../context/CartContext/CartContext"
-
+import { faMagnifyingGlassPlus } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export const ItemDetail = ( {item} ) => {
 
     
-    const [cantidad, setCantidad] = useState(1)
+    const [cantidad, setCantidad] = useState(1);
 
-    const { agregarAlCarrito, isInCar } = useContext(CartContext)
+    const [zoomVisible, setZoomVisible] = useState(false);
+
+    const { agregarAlCarrito, isInCar } = useContext(CartContext);
+
+    const handleZoomImg = () => {
+        setZoomVisible(true);
+    }
+    
+    const handleExitZoom = () => {
+        setZoomVisible(false);
+    }
 
     console.log(isInCar(item.id))
 
@@ -20,7 +31,6 @@ export const ItemDetail = ( {item} ) => {
     const handleVolver = () => {
         navigate(-1)
     }
-
     
     const handleAgregar = () => {
         const newItem =  {
@@ -33,8 +43,37 @@ export const ItemDetail = ( {item} ) => {
 
     return (
         <div className="contenedor_item_detail">
+            {
+                zoomVisible 
+                ? <div className="zoomImg">
+                    <div className="contenedorSalida">
+                        <div className="contenedorCruzZoom">
+                            <FontAwesomeIcon onClick={ handleExitZoom } className="cruzZoom" icon={ faXmark }/>
+                        </div>
+                    </div>
+                    <div className="contenedorImgZoom">
+                        <div className="contenedorInternoImagenZoom">
+                            <img src={item.img} alt={item.name}/>
+                        </div>
+                    </div>
+                </div>
+                : <div></div>
+            }
             <div className="itemImg">
+                <div className="contenedorDetallesDeImg">
+                    <div className="contenedorInternoDetallesImg">
+                        <div className="contenedorPalabraHome">
+                            <h6>Home</h6>
+                        </div>
+                        <div className="contenedorPalabraAuriculares">
+                            <h6>Auriculares</h6>
+                        </div>
+                    </div>
+                </div>
                 <div className="contenedor-img">
+                    <button onClick={ handleZoomImg } className="contenedorLupaImg">
+                        <FontAwesomeIcon className="lupaImg" icon={ faMagnifyingGlassPlus }/>
+                    </button>
                     <img src={item.img} alt={item.name}/>
                 </div>
             </div>
